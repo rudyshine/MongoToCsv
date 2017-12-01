7ab7c75d367f772705f21e74c4f186a35c1f6b92
rudyz <784456700@qq.con>
2017-12-01 17:48:23 +0800

    从MongoDB读数据到CSV

--- MTCsv.py
+++ MTCsv.py
@@ -14,18 +14,34 @@ db = client[db_name]
 # for i in set.find():
 #     print(i)
 
-#从mongoDB数据库中读取Cheaa_Info_Data数据写到CSV文件里面
+# #从mongoDB数据库中读取Cheaa_Info_Data数据写到CSV文件里面
+# def WriteCheaaCsv():
+#     File=codecs.open('Cheaa_Info_Data.csv','w')  ##建立并打开文件
+#     # headList=['Time','Title','ArtcleContent','startUrl','InfoFrom','LinkUrl']  ##表头
+#     headList=['ArtcleContent']  ##表头
+#     writer=csv.writer(File)   ##写入文件
+#     writer.writerow(headList)  ##写入 标题
+#     # headList[0] ='_id'
+#     for info in db.Cheaa_Info_Items.find({'Title':{'$regex':'/京东.*/i'}}):##查到数据库里全部的数据
+#         vList = []
+#         for k in headList:
+#             vList.append(info[k])
+#             writer.writerow(vList)
+#
+# WriteCheaaCsv()
+
+#从mongoDB数据库中读取Cheaa_Info_Data数据写到doc文件里面,一条记录一个文件
 def WriteCheaaCsv():
-    File=codecs.open('Cheaa_Info_Data.csv','w')  ##建立并打开文件
-    headList=['Time','Title','ArtcleContent','startUrl','InfoFrom','LinkUrl']  ##表头
-    writer=csv.writer(File)   ##写入文件
-    writer.writerow(headList)  ##写入 标题
-    # headList[0] ='_id'
-    for info in db.Cheaa_Info_Items.find(): ##查到数据库里全部的数据
+    for info in db.Cheaa_Info_Items.find():  ##查到数据库里全部的数据
         vList = []
+        headList = ['ArtcleContent'][0]  ##表头
+        infoName=info['Title']
+        print(infoName)
+        File = codecs.open('%s.doc'%infoName,'w')  ##建立并打开文件
+        writer = csv.writer(File)  ##写入文件
+        writer.writerow(headList)  ##写入 标题
         for k in headList:
             vList.append(info[k])
             writer.writerow(vList)
 
 WriteCheaaCsv()
-
