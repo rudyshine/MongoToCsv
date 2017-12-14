2ee8722f2a24f6dd63d5b0b1446ead3c764bf98c
rudyz <784456700@qq.con>
2017-12-14 10:52:21 +0800

    从MongoDB读数据到CSV

--- MTCsv.py
+++ MTCsv.py
@@ -6,22 +6,23 @@ import codecs
 #建立连接
 client = MongoClient('172.28.161.186',27017)
 #client = MongoClient(‘10.20.66.106‘, 27017)
-db_name ='CHEAA' #数据库名
+# db_name ='CHEAA' #数据库
+db_name ='WeatherData' #数据库
 db = client[db_name]
-# set=db.Cheaa_Info_Items
+set=db.HistoryData
 
 # #测试数据库是否连接（查全部数据）
 # for i in set.find():
 #     print(i)
 
-#从mongoDB数据库中读取Cheaa_Info_Data数据写到CSV文件里面
+###从mongoDB数据库中读取Cheaa_Info_Data数据写到CSV文件里面
 def WriteCheaaCsv():
     File=codecs.open('Cheaa_Info_Data.csv','w')  ##建立并打开文件
-    headList=['Time','Title','ArtcleContent','startUrl','InfoFrom','LinkUrl']  ##表头
+    headList=['日期', '最高温度', '最低温度', '湿度','节日', '星期', '发布时间','idNumber']  ##表头
     # headList=['ArtcleContent']  ##表头
     writer=csv.writer(File)   ##写入文件
     writer.writerow(headList)  ##写入 标题
-    for info in db.Cheaa_Info_Items.find():##查到数据库里全部的数据
+    for info in set.find():##查到数据库里全部的数据
         vList = []
         for k in headList:
             vList.append(info[k])
